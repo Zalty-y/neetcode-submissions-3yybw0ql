51cd5f46cb16e7a8c32f2224ac62a41dffc95025
Zalty-y <83557045+Zalty-y@users.noreply.github.com>
2026-04-06 22:13:17 +0200

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.java
@@ -0,0 +1,48 @@
+class Solution {
+    private String nullString = "null";
+    private String empty = "";
+    private char separator = '#';
+
+    public String encode(List<String> strs) {
+        if (strs == null || strs.isEmpty()) {
+            return nullString;
+        } 
+
+        StringBuilder sb = new StringBuilder();
+
+        for (String str : strs) {
+            sb.append(str.length()).append(separator).append(str);
+        }
+
+        return sb.toString();
+    }
+
+    public List<String> decode(String str) {
+        List<String> result = new ArrayList();
+
+        if (str.equals(nullString)) {
+            return result;
+        }
+
+        int left = 0;
+        int right = 0;
+
+        while (right < str.length()) {
+            if (str.charAt(right) != separator) {
+                right++;
+                continue;
+            }
+
+            int length = Integer.parseInt(str.substring(left, right));
+            right++;
+
+            String substring = length == 0 ? empty : str.substring(right, right + length);
+            result.add(substring);
+
+            right += length;
+            left = right;
+        }
+
+        return result;
+    }
+}
